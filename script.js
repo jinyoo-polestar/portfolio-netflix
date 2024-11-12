7e8bf69f94ee63d61acc9a0c76772ced7e242f2e
jinyoo.shin <jinyoo.polestar@gmail.com>
2024-11-13 01:14:03 +0900

    #style: code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// Email Form Controller
+// Main, Support - Email form css controller
 const $signupEmailInputs = document.querySelectorAll('.signup__input');
 const $signupEmailLabels = document.querySelectorAll('.signup__label');
+const $sliderWrapper = document.querySelector('.trend__slider');
+const $leftBtn = document.querySelector('.slider__left-btn');
+const $rightBtn = document.querySelector('.slider__right-btn');
 
 $signupEmailInputs.forEach(($input, index) => {
   const $signupEmailLabel = $signupEmailLabels[index];
@@ -15,7 +18,7 @@ $signupEmailInputs.forEach(($input, index) => {
 });
 
 
-// Q&A Section - Toggle Answer Visibility
+// Q&A - Toggle answer visibility controller
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('qna__question')) {
     const questionItem = event.target.closest('.qna__item'); 
@@ -28,3 +31,30 @@ document.addEventListener('click', (event) => {
     event.target.classList.add('active');
   }
 })
+
+// Slider - Show/Hide left,right button by scroll width
+function setSliderButtonsVisible() {
+    const currentScrollPosition = $sliderWrapper.scrollLeft;
+    const maxScrollPosition = $sliderWrapper.scrollWidth - $sliderWrapper.clientWidth; 
+
+    $leftBtn.style.display = currentScrollPosition === 0 ? 'none' : 'flex';
+    $rightBtn.style.display = currentScrollPosition >= maxScrollPosition ? 'none' : 'flex';
+}
+
+$leftBtn.addEventListener('click', function() {
+    $sliderWrapper.scrollBy({ left: -$sliderWrapper.clientWidth, behavior: 'smooth' });
+});
+
+$rightBtn.addEventListener('click', function() {
+    const remainingScroll = $sliderWrapper.scrollWidth - $sliderWrapper.scrollLeft - $sliderWrapper.clientWidth;
+    
+    if (remainingScroll < $sliderWrapper.clientWidth) {
+        $sliderWrapper.scrollBy({ left: remainingScroll, behavior: 'smooth' });
+    } else {
+        $sliderWrapper.scrollBy({ left: $sliderWrapper.clientWidth, behavior: 'smooth' });
+    }
+});
+
+$sliderWrapper.addEventListener('scroll', setSliderButtonsVisible);
+
+setSliderButtonsVisible();
